e5a150302d6ec44da97d08a4b57e41c76554959f
Alex Panov <alexeypd@gmail.com>
2019-08-09 10:30:44 -0400

    Jenkins: working jenkins, local mysql

--- src/main/java/com/ms3/bootcamp/SpaceShipApp.java
+++ src/main/java/com/ms3/bootcamp/SpaceShipApp.java
@@ -2,8 +2,10 @@ package com.ms3.bootcamp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootApplication
+//@ActiveProfiles("internal")
 public class SpaceShipApp {
     public static void main(String[] args) {
         SpringApplication.run(SpaceShipApp.class, args);
