7927f7bacdbd83bade932ff0ef4e2f624f94b8e1
Alex Panov <alexeypd@gmail.com>
2019-08-12 08:23:35 -0400

    BAuth enabled mysql driver last version fixed

--- src/main/java/com/ms3/bootcamp/authentication/Role.java
+++ src/main/java/com/ms3/bootcamp/authentication/Role.java
@@ -1,12 +1,12 @@
-//package com.ms3.bootcamp.authentication;
-//
-//import org.springframework.security.core.GrantedAuthority;
-//
-//public enum Role implements GrantedAuthority {
-//    USER,
-//    ADMIN;
-//
-//    public String getAuthority() {
-//        return name();
-//    }
-//}
+package com.ms3.bootcamp.authentication;
+
+import org.springframework.security.core.GrantedAuthority;
+
+public enum Role implements GrantedAuthority {
+    USER,
+    ADMIN;
+
+    public String getAuthority() {
+        return name();
+    }
+}
--- src/main/java/com/ms3/bootcamp/authentication/UserService.java
+++ src/main/java/com/ms3/bootcamp/authentication/UserService.java
@@ -1,21 +1,21 @@
-//package com.ms3.bootcamp.authentication;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.security.core.userdetails.UserDetails;
-//import org.springframework.security.core.userdetails.UserDetailsService;
-//import org.springframework.security.core.userdetails.UsernameNotFoundException;
-//import org.springframework.stereotype.Service;
-//
-//@Service
-//public class UserService implements UserDetailsService {
-//
-//    @Autowired
-//    private UserRepository userRepository;
-//
-//    @Override
-//    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
-//        UserDetails userDetails = (UserDetails) userRepository.findTopByUsername(userName);
-//        if (userDetails==null) throw new UsernameNotFoundException("User not found in the DB");
-//        return userDetails;
-//    }
-//}
+package com.ms3.bootcamp.authentication;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+        UserDetails userDetails = (UserDetails) userRepository.findTopByUsername(userName);
+        if (userDetails==null) throw new UsernameNotFoundException("User not found in the DB");
+        return userDetails;
+    }
+}
